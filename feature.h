bf1a3cfa9bce03089808153f051b319494ce8474
rangga <ranggaasaatriaa@gmail.com>
2025-12-24 20:20:03 +0700

    Fix header guards and clean up feature.h

--- feature.h
+++ feature.h
@@ -11,15 +11,14 @@ typedef struct {
     char time[30];
 } Feature;
 
-// External declarations
 extern Feature *features;
 extern int activityCount;
 
-// Function prototypes
 void to_do_list();
 void view_activity();
 void activity_menu();
 void show_stack_view();
 void update_progress(int *progress); 
+
 
 #endif
